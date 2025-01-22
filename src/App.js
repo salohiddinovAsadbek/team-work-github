6cfef1d0ca3001375c09f7fafec3c501891b4ffd
salohiddinovAsadbek <mrxcapitain@gmail.com>
2025-01-22 17:25:10 +0500

    always

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Always from "./always";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Always />
+    </div>
+  );
 }
 
 export default App;
