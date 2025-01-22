1cc1d36e34b724a8287a38d1a0d7c249c906c1c7
Sherzodbek1020 <nazirjonnazirjon70@gmail.com>
2025-01-22 17:26:17 +0500

    Choose

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Choose from "./Choose";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Choose />
+    </div>
+  );
 }
 
 export default App;
