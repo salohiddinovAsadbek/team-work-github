d3dc9d0c01c7a65f8c405d495e61cb32e506771d
salohiddinovAsadbek <mrxcapitain@gmail.com>
2025-01-22 18:08:19 +0500

    psuh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import Choose from "./Choose";
+// import Choose from "./Choose";
 import Magic from "./Magic";
-import React from 'react';
-import Navbar from './Navbar/Navbar';
-import './styles/style.css';
-import Home from '../src/home'
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import "./styles/style.css";
+import Home from "../src/home";
 import Choose from "./Choose";
 import Always from "./always";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Home />
       <Choose />
-      <Magic />
-      <Navbar /> 
-    <Home/>
-    <Choose />
       <Always />
+      <Magic />
     </div>
   );
 }
